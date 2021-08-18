499fc2b37ec2877754e5786849538f0afaaa8bf5
faeq209 <faeq.hussain.1860@gmail.com>
2021-08-18 23:56:01 +0530

    healthbar

---
+++ zombies-game/Assets/healthbar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthbar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public void SETmAXhEALTH(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+    }
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+
+}
+
