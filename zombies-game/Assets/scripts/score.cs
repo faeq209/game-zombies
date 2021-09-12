76d4f53409a7110db93706c0c1ef8cd697f440c0
DevkotaSaksham <sakxamdev@gmail.com>
2021-09-12 16:19:29 +0545

    new settings of game
    -added enemies
    -enemy prefab is instantiate(for now) in certain intervals
    -losing text is displayed when health bar reacher 0
    -added some classical music in background

---
+++ zombies-game/Assets/scripts/score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public float t;
+    // Start is called before the first frame update
+  
+    public Text points;
+    void Start()
+    {
+        t = 0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        points.text ="Score:"+(int)t;
+        t = t + 1 * Time.deltaTime;
+    }
+}
