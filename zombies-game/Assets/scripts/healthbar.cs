ab1cced58d137c61c6370a362d9d87715b2bdd96
DevkotaSaksham <sakxamdev@gmail.com>
2021-09-12 15:05:13 +0545

    enemy addition and others

---
+++ zombies-game/Assets/scripts/healthbar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthbar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+   
+    public void setmaxhealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+        fill.color = gradient.Evaluate(1f);
+    }
+    public void sethealth(float health)
+    {
+        health -= 1;
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+    public void damage(int health)
+    {
+        health -= 1;
+    }
+        
+}
+
