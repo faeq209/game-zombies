67c3b3a79be29200553be0eac456876d5e8afe4d
DevkotaSaksham <sakxamdev@gmail.com>
2021-09-16 19:44:28 +0545

    new mechanism for 2d ai tracking
    used namesh plus for 2d tracking

---
+++ zombies-game/Assets/scripts/enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy : MonoBehaviour
+{
+    private Transform target;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.transform.position);
+    }
+}
